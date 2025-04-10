dbc8d78d5d197b1a1c83c5e63b523c66b85b1ab1
eliyajoseph7 <exaveryeliya20@gmail.com>
2025-04-10 17:15:34 +0300

    modified

--- todo-backend/src/main/java/com/todoapplication/todo/services/TodoService.java
+++ todo-backend/src/main/java/com/todoapplication/todo/services/TodoService.java
@@ -2,7 +2,7 @@
 package com.todoapplication.todo.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+// import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
